3a5425423f145c9e82d5312108dd5f25bb3f65e6
royliu <roy.liu@grapecity.com>
2017-07-06 15:57:47 +0800

    kanji to kana converter

---
+++ KanjiToKana/KanjiToKanaConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanjiToKana
+{
+    public enum Mode
+    {
+        FullWidthKana = 0,
+        HalfWidthKana = 1,
+    }
+
+    public static class KanjiToKanaConverter
+    {
+        public static string Convert(string source, Mode mode)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                return source;
+            }            
+
+            GlobalInterface converter = new GlobalInterface();
+
+            if (mode == Mode.HalfWidthKana)
+            {
+                return converter.GetConversion(
+                    "MsIme.Japan", 
+                    GlobalInterface.FELANG_REQUEST.FELANG_REQ_REV, 
+                    GlobalInterface.FELANG_CMODE.FELANG_CMODE_KATAKANAOUT, 
+                    source);
+            }
+            else if (mode == Mode.FullWidthKana)
+            {
+                string fullwidth = converter.GetConversion(
+                    "MsIme.Japan",
+                    GlobalInterface.FELANG_REQUEST.FELANG_REQ_REV,
+                    GlobalInterface.FELANG_CMODE.FELANG_CMODE_KATAKANAOUT,
+                    source);
+                return converter.GetHalfWidthKana(fullwidth);
+            }
+            else
+            {
+                throw new InvalidOperationException("not support mode");
+            }            
+        }
+    }
+}
---
+++ KanjiToKanaRunner/Program.cs
@@ -0,0 +1,29 @@
+﻿using KanjiToKana;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanjiToKanaRunner
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length != 1)
+            {
+                Console.WriteLine("use source string as the only parameter.");
+                return;
+            }            
+
+            string source = args[0];
+            string result = KanjiToKanaConverter.Convert(source, Mode.FullWidthKana);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
